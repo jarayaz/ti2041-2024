6a50ba787f4dea2df2ffc0166f6517aa2fdc854c
JAIR ARAYA <164426915+jarayaz@users.noreply.github.com>
2024-09-22 22:05:25 -0300

    final

--- evaluaciones/evaluacion1/gestionAPP/gestionAPP/asgi.py
+++ evaluaciones/evaluacion1/gestionAPP/gestionAPP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gestionAPP project.
+ASGI config for AppGestion project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionAPP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppGestion.settings')
 
 application = get_asgi_application()
--- evaluaciones/evaluacion1/gestionAPP/gestionAPP/wsgi.py
+++ evaluaciones/evaluacion1/gestionAPP/gestionAPP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gestionAPP project.
+WSGI config for AppGestion project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionAPP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppGestion.settings')
 
 application = get_wsgi_application()
+
